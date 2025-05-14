a0e9885c9c35c8803ae7f7ea31495fb4f93e06bb
RaviTejaCh-dev <66156436+RaviTejaCh-dev@users.noreply.github.com>
2025-05-13 17:23:03 -0700

    Time: 16 ms (48.52%), Space: 52.6 MB (39.92%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.java
@@ -0,0 +1,42 @@
+class MyHashSet {
+
+    private static final int SIZE = 769; // a prime number for better distribution
+    private LinkedList<Integer>[] buckets;
+
+    public MyHashSet() {
+        buckets = new LinkedList[SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(int key) {
+        return key % SIZE;
+    }
+
+    public void add(int key) {
+        int index = hash(key);
+        LinkedList<Integer> bucket = buckets[index];
+        if (!bucket.contains(key)) {
+            bucket.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int index = hash(key);
+        buckets[index].remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        int index = hash(key);
+        return buckets[index].contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
