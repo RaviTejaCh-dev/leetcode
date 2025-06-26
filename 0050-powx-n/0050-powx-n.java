158e2906ba797e219536faea1a12ac29106214d6
RaviTejaCh-dev <66156436+RaviTejaCh-dev@users.noreply.github.com>
2025-06-25 18:31:47 -0700

    Time: 0 ms (100%), Space: 41.9 MB (88.18%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+
+        double re = myPow(x, n/2);
+        if(n < 0){
+            x = 1/x;
+        } 
+        if (n%2 == 0){
+            return re * re;
+        } else {
+            return re * re * x;
+        }
+    }
+}
